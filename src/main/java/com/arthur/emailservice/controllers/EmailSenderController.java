62471924cb92ae990ff1628b70c3f4afe17fb7bc
Arthur-so <arthursantos.oliveira001@gmail.com>
2023-11-03 21:21:44 -0300

    feat: email sender controller (Post)

---
+++ src/main/java/com/arthur/emailservice/controllers/EmailSenderController.java
@@ -0,0 +1,34 @@
+package com.arthur.emailservice.controllers;
+
+import com.arthur.emailservice.application.EmailSenderUseCaseImpl;
+import com.arthur.emailservice.core.EmailRequest;
+import com.arthur.emailservice.core.exceptions.EmailSenderException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderUseCaseImpl emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderUseCaseImpl emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
+        try {
+            emailSenderService.sendEmail(emailRequest.toEmail(), emailRequest.subject(), emailRequest.body());
+            return ResponseEntity.ok("Email sent successfully.");
+        } catch(EmailSenderException emailSenderException){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email sending failed.");
+        }
+    }
+}
